2662a4e8931a16cd9ccbcc204085e7b3f4972ff5
ZachSaucier <zachsaucier@gmail.com>
2024-04-05 07:45:14 -0400

    Fixed layout params import

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,17 +1,18 @@
 import { error } from '@sveltejs/kit';
 
-export const prerender = true;
-export const trailingSlash = 'always';
-
 // Allows client side routing. Necessary for page transitions and link prefetching; change to false if you prefer ordinary routing without JS
 export const csr = true;
+export const prerender = true;
 
-export const load = async ({ url, fetch }) => {
+export const trailingSlash = 'always';
+
+export const load = async ({ data, url, fetch }) => {
   const recent_posts_res = await fetch(`${url.origin}/blog/api/recent-posts.json`);
   const recent_posts = await recent_posts_res.json();
 
   try {
     return {
+      ...data,
       path: url.pathname,
       recent_posts: recent_posts,
     };
