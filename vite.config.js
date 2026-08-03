09d8137203be30f854379e1212e53780da0f1b2d
ZachSaucier <zachsaucier@gmail.com>
2026-08-02 21:57:07 -0400

    Fixing visual issue with build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { Features } from 'lightningcss';
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  css: {
+    lightningcss: {
+      // The light-dark() polyfill breaks values stored in custom properties and in
+      // shorthands like border. This site uses color-scheme + native light-dark().
+      exclude: Features.LightDark,
+    },
+  },
   server: {
     fs: {
       allow: ['.'],
